f8db2b674501f71190e6be9aa9dabb9d47cefed0
Adam Gregořica <agregorica@gmail.com>
2024-10-25 23:54:14 +0200

    Added bluetooth devices readout Right now only supports normal bluetooth devices, ,bluetooth LE would help

--- app/src/main/java/eu/gregstr/peoplecounter/MainActivity.java
+++ app/src/main/java/eu/gregstr/peoplecounter/MainActivity.java
@@ -1,16 +1,136 @@
 package eu.gregstr.peoplecounter;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothManager bluetoothManager;
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher, as an instance variable.
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello there", Toast.LENGTH_SHORT).show();
+
+        Button searchButton = findViewById(R.id.Search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Searching", Toast.LENGTH_SHORT).show();
+                System.out.println(scanForDevices());
+            }
+        });
+
+        bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "Device doesn't support bluetooth!", Toast.LENGTH_SHORT).show();
+        }
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_SCAN);
+        }
+
+        scanForDevices();
+
+    }
+
+    private boolean scanForDevices() {
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!isGpsEnabled) {
+        }
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "Permission for bluetooth scanning denied!", Toast.LENGTH_SHORT).show();
+            requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_SCAN);
+            return false;
+        }
+        System.out.println(bluetoothAdapter.getScanMode());
+        System.out.println(BluetoothAdapter.SCAN_MODE_NONE);
+
+        return bluetoothAdapter.startDiscovery();
+    }
+
+    protected void AddToList(String name) {
+        LinearLayout device_list = findViewById(R.id.device_list);
+
+        TextView device = new TextView(this);
+        device.setText(name);
+        device_list.addView(device);
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                System.out.println(deviceHardwareAddress);
+                AddToList(deviceHardwareAddress);
+            }
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
     }
 }
