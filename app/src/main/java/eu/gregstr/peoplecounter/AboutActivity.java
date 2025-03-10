d15ed56f83910a41e41506c7f80dabd5dc0e8945
a.gregorica.st <agregorica@gmail.com>
2025-03-10 10:32:32 +0100

    Added HTTP request for is birthday API

--- app/src/main/java/eu/gregstr/peoplecounter/AboutActivity.java
+++ app/src/main/java/eu/gregstr/peoplecounter/AboutActivity.java
@@ -5,11 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.util.JsonUtils;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.*;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+
 public class AboutActivity extends NavigationActivity {
+
+    OkHttpClient client = new OkHttpClient();
+    public String url= "https://gregstr.eu/is-it-my-birthday.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,34 @@ public class AboutActivity extends NavigationActivity {
         setContentView(R.layout.activity_about);
         SetBottomMenu(R.id.about);
         SetActionBar(getString(R.string.about), true);
+
+        Request get = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(get).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                try {
+                    ResponseBody responseBody = response.body();
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Unexpected code " + response);
+                    }
+                    JSONObject jsonObject = new JSONObject(responseBody.string());
+
+
+
+                    System.out.println(jsonObject.get("birthday"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         Button githubBtn = findViewById(R.id.GithubButton);
 
@@ -29,6 +77,7 @@ public class AboutActivity extends NavigationActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
